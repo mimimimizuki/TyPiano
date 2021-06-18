7a6873c6cfdefc8d361d368abb60586ba231984f
chum0n <cyumon_tkt.0914@icloud.com>
2021-06-19 02:54:54 +0900

    return nodeStr

--- muda-front/src/consoleKeyEnter.tsx
+++ muda-front/src/consoleKeyEnter.tsx
@@ -14,11 +14,13 @@ async function sleep(msec:any) {
 type nowGotted = "d" | "r" | "m" | "f" | "s" | "sh" | "none";
 type note = "do" | "re" | "mi" | "fa" | "so" | "ra" | "shi" | "none";
 
-const ReceiveKeyEnter: React.FC = () => {
+const ConsoleKeyEnter: React.FC = () => {
   const [receivedKey, setReceivedKey] = useState<nowGotted>("none");
   const [note, setNote] = useState<note>("none");
-  var noteStr = ""
-  var noteABCStr = ""
+  const [noteStr, setNoteStr] = useState<string>("");
+  const [noteABCStr, setNoteABCStr] = useState<string>("");
+  var tmpNoteStr = ""
+  var tmpNoteABCStr = ""
   var noteTypeArray = ["do", "re", "mi", "fa", "so", "ra", "shi"]
 
   const d = 68;
@@ -38,69 +40,63 @@ const ReceiveKeyEnter: React.FC = () => {
       const keyCode = event.keyCode;
       if (keyCode === enter) {
         var pointer = 0
-        while (pointer+1 < noteStr.length) {
-            if (noteStr[pointer] === " ") {
+        while (pointer+1 < tmpNoteStr.length) {
+            if (tmpNoteStr[pointer] === " ") {
                 sleep(duration/1000)
             } else {
-                var oneNote = noteStr.substr(pointer, 2)
+                var oneNote = tmpNoteStr.substr(pointer, 2)
                 if (oneNote === "sh" || oneNote[0] === "#") {
-                    oneNote += noteStr[pointer+2]
+                    oneNote += tmpNoteStr[pointer+2]
                     pointer += 3
                 } else {
                     pointer += 2
                 }
                 if (oneNote in noteTypeArray) {
-                    noteABCStr += toABC(oneNote)
+                    tmpNoteABCStr += toABC(oneNote)
+                    setNoteABCStr(tmpNoteABCStr)
                     playNote(oneNote)
                 }
             }
         }
-        noteStr = ""
-        setReceivedKey("none");
+        tmpNoteStr = ""
       } else if (keyCode === d) {
-        noteStr += "d"
-        setReceivedKey("d");
+        tmpNoteStr += "d"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === r) {
-        noteStr += "r"
-        setReceivedKey("r");
+        tmpNoteStr += "r"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === m) {
-        noteStr += "m"
-        setReceivedKey("m");
+        tmpNoteStr += "m"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === f) {
-        noteStr += "f"
-        setReceivedKey("f");
+        tmpNoteStr += "f"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === s) {
-        noteStr += "s"
-        setReceivedKey("s");
+        tmpNoteStr += "s"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === h && receivedKey === "s") {
-        noteStr += "h"
-        setReceivedKey("sh");
+        tmpNoteStr += "h"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === o && receivedKey === "d") {
-        noteStr += "o"
-        setNote("do");
-        setReceivedKey("none");
+        tmpNoteStr += "o"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === o && receivedKey === "s") {
-        noteStr += "o"
-        setNote("so");
-        setReceivedKey("none");
+        tmpNoteStr += "o"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === e && receivedKey === "r") {
-        noteStr += "e"
-        setNote("re");
-        setReceivedKey("none");
+        tmpNoteStr += "e"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === i && receivedKey === "m") {
-        noteStr += "i"
-        setNote("mi");
-        setReceivedKey("none");
+        tmpNoteStr += "i"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === i && receivedKey === "sh") {
-        noteStr += "i"
-        setNote("shi");
-        setReceivedKey("none");
+        tmpNoteStr += "i"
+        setNoteStr(tmpNoteStr)
       } else if (keyCode === a && receivedKey === "f") {
-        noteStr += "a"
-        setNote("fa");
-        setReceivedKey("none");
+        tmpNoteStr += "a"
+        setNoteStr(tmpNoteStr)
       } else {
-        setReceivedKey("none");
+        // setReceivedKey("none");
       }
     }
 
@@ -108,9 +104,9 @@ const ReceiveKeyEnter: React.FC = () => {
     return () => {
       document.removeEventListener("keydown", setFromNone);
     };
-  }, [receivedKey]);
+  }, [noteABCStr]);
 
-  return <div>{note}</div>;
+  return <div>{noteStr}</div>;
 };
 
-export default ReceiveKeyEnter;
+export default ConsoleKeyEnter;
