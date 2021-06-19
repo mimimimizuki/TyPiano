2603e3150dc5ede0e90d4a019fdc4175ab07b314
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-06-19 13:42:42 +0900

    fix: use state

--- muda-front/src/consoleMode.tsx
+++ muda-front/src/consoleMode.tsx
@@ -1,36 +1,62 @@
-import React ,{ useState } from "react";
-import { emitKeypressEvents } from "readline";
-import { Z_FIXED } from "zlib";
-import ReceiveKeyEnter from "./receiveKeyEnter";
+import React from "react";
+import abcjs from "abcjs";
+import useConsoleKeyEnter from "./useConsoleKeyEnter";
 
 const consoleColor = {
   backgroundColor: "#041344",
-  color:"#FFFFFF",
+  color: "#FFFFFF",
   height: "60vh",
   paddingRight: 20,
   paddingLeft: 20,
   paddingTop: 5,
   paddingBottom: 5,
+  overflowY: "scroll" as "scroll",
 };
 
+const ConsoleBlock: React.FC<{ command: string; abc: string; i: number }> = ({
+  command,
+  abc,
+  i,
+}) => {
+  const commandId = "command" + String(i);
+  const scoreId = "score" + String(i);
 
-
-const ConsoleMode: React.FC = () => {
-
-  // 今まで入力したコマンドのリスト
-  const command=["do","re","mi"];
-  // 画面に表示するもの
-  const list = [];
-
-  for(let i=0; i<command.length; i++){
-     list.push(<p>$ {command[i]}</p>)    
-  }
+  abcjs.renderAbc(scoreId, abc);
 
   return (
     <div>
-      <ReceiveKeyEnter></ReceiveKeyEnter>
+      <p key={commandId}>$ {command}</p>
+      <div id={scoreId} key={scoreId}></div>
+    </div>
+  );
+};
+
+const ConsoleMode: React.FC = () => {
+  // [["doremi", "remifa"], ["CDE", "EFG"], "dor"のイメージ
+  // const [commandHist, ABCHist, nowString] = useConsoleKeyEnter();
+  const [ABC, nowString] = useConsoleKeyEnter();
+  //今だけ
+  const ABCHist = [ABC];
+
+  // 画面に表示するもの
+  const list = [];
+
+  // 今までの履歴をデザインに変えている
+  for (let i = 0; i < ABCHist.length; i++) {
+    // ConsoleBlockは、$doremiと五線譜のブロック
+    // commandは$doremiに使う
+    // abcは五線譜に使う
+    // iはidとかなので気にしなくていい
+    list.push(
+      <ConsoleBlock command={ABCHist[i]} abc={ABCHist[i]} i={i}></ConsoleBlock>
+    );
+  }
+  // 履歴と、一番下に今のやつを表示
+  return (
+    <div>
       <div style={consoleColor}>
         {list}
+        <p>$ {nowString}</p>
       </div>
     </div>
   );
