21a456954e8971607fbf70e739d95225e9b1360b
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-06-20 05:15:00 +0900

    add: top page

--- muda-front/src/App.tsx
+++ muda-front/src/App.tsx
@@ -3,43 +3,58 @@ import "./App.css";
 import Header from "./Header";
 import ConsoleMode, { ConsoleTab } from "./consoleMode";
 import PianoMode, { PianoBase } from "./pianoMode";
+import Top from "./Top";
 
 function App() {
+  const [showMain, setShowMain] = useState<boolean>(false);
   const [showConsole, setShowConsole] = useState<boolean>(true);
-  const [finShowConsole, setFinShowConsole] = useState<boolean>(true);
-  const [showPiano, setShowPiano] = useState<boolean>(false);
+  const [finShowConsole, setFinShowConsole] = useState<boolean>(false);
+  const [showPiano, setShowPiano] = useState<boolean>(true);
   const [finShowPiano, setFinShowPiano] = useState<boolean>(false);
 
+  function toConsole() {
+    setShowMain(true);
+    // コンソールを広げる
+    setFinShowPiano(false);
+    setShowConsole(true);
+    // ピアノを縮める
+    setShowPiano(false);
+    // ピアノを縮め切ったら消す
+    setTimeout(() => {
+      setFinShowConsole(true);
+    }, 2000);
+  }
+  function toPiano() {
+    setShowMain(true);
+    // コンソールを縮める
+    setShowConsole(false);
+    setFinShowConsole(false);
+    // ピアノを広げる
+    setShowPiano(true);
+    // コンソールを縮め切ったら消す
+    setTimeout(() => {
+      setFinShowPiano(true);
+    }, 2000);
+  }
   function handleClick() {
     if (showConsole) {
-      // コンソールを縮める
-      setShowConsole(false);
-      setFinShowConsole(false);
-      // ピアノを広げる
-      setShowPiano(true);
-      // コンソールを縮め切ったら消す
-      setTimeout(() => {
-        setFinShowPiano(true);
-      }, 2000);
+      toPiano();
     } else {
-      // コンソールを広げる
-      setFinShowPiano(false);
-      setShowConsole(true);
-      // ピアノを縮める
-      setShowPiano(false);
-      // ピアノを縮め切ったら消す
-      setTimeout(() => {
-        setFinShowConsole(true);
-      }, 2000);
+      toConsole();
     }
   }
   return (
-    <div className="App" onClick={handleClick}>
-      <Header />
-      {!finShowPiano && <ConsoleMode doRemove={!showConsole} />}
-      <ConsoleTab inConsole={finShowConsole} />
-      {!finShowConsole && <PianoMode doRemove={!showPiano} />}
-      <PianoBase inPiano={finShowPiano} />
+    <div className="App">
+      {!showMain && <Top piano={toPiano} console={toConsole} />}
+      {showMain && <Header />}
+      {showMain && (
+        <div onClick={handleClick}>
+          {!finShowPiano && <ConsoleMode doRemove={!showConsole} />}
+          <ConsoleTab inConsole={finShowConsole} />
+          {!finShowConsole && <PianoMode doRemove={!showPiano} />}
+          <PianoBase inPiano={finShowPiano} />
+        </div>
+      )}
     </div>
   );
 }
