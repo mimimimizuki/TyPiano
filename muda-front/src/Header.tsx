419bea6d6bcdddbf4c5bf4d38a2c1ab15e98f08c
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-06-20 11:47:09 +0900

    style: logo and pic

--- muda-front/src/Header.tsx
+++ muda-front/src/Header.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 const headerStyles = {
+  display: "flex",
+  arignItems: "center",
   width: "100%",
-  backgroundColor: "#ADF7AE",
+  height: 40,
+  backgroundColor: "#C6F7C7",
   color: "#243324",
   fontFamily: "Alex Brush, cursive",
   fontSize: 22,
@@ -10,7 +13,12 @@ const headerStyles = {
 };
 
 const Header: React.FC = () => {
-  return <div style={headerStyles}>TyPiano</div>;
+  return (
+    <div style={headerStyles}>
+      <img src="/icon.png" alt="" style={{ height: 35 }}></img>
+      <span>TyPiano</span>
+    </div>
+  );
 };
 
 export default Header;
